7e79a675e03d57f457bffedbeea33062fa75a91b
kayleeWei <weimin19940508@126.com>
2021-07-07 10:56:04 +0800

    add: 爬楼梯

---
+++ leetcode/DynamicProgramming/1-爬楼梯问题.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+  const size = cost.length;
+  const dp = new Array(size);
+  dp[0] = cost[0];
+  dp[1] = cost[1];
+  for(let i = 2; i < size; i++) {
+    dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+  }
+  return Math.min(dp[size - 1], dp[size - 2]);
+
+};
