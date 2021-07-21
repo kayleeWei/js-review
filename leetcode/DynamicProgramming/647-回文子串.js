f6cfad63f32d46b1588e5fd6200d3cb92c82e34a
kayleeWei <weimin19940508@126.com>
2021-07-21 23:11:26 +0800

    add: 回文子串中心扩展法

--- leetcode/DynamicProgramming/647-回文子串.js
+++ leetcode/DynamicProgramming/647-回文子串.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 //  https://leetcode-cn.com/problems/palindromic-substrings/solution/647-hui-wen-zi-chuan-dong-tai-gui-hua-zh-6f7n/
+// dp
 var countSubstrings = function(s) {
    const size = s.length;
    const dp = new Array(size);
@@ -11,6 +12,7 @@ var countSubstrings = function(s) {
    }
    let result = 0;
 
+   // 从下往上，从左往右遍历上三角矩阵
    for(let i = size - 1; i >= 0; i--) {
       for(let j = i; j < size; j++) {
          if (i === 4 && j === 5) {
@@ -29,5 +31,27 @@ var countSubstrings = function(s) {
    }
    return result;
 };
+
+// 中心扩展法
+// https://leetcode-cn.com/problems/palindromic-substrings/solution/hui-wen-zi-chuan-by-leetcode-solution/
+var countSubstrings = function(s) {
+   let result = 0;
+   const size = s.length;
+
+   // 一共有 2 * size - 1 个中心，回文中心左右的起始位置分别为⌊2/i⌋, ⌊2/i⌋ + (i mod 2)
+   for(let i = 0; i < 2 * size - 1; i++) {
+      let l = Math.floor(i / 2);
+      let r = Math.floor(i / 2) + i % 2;
+
+      while(l >= 0 && r < size && s[l] === s[r]) {
+         l--;
+         r++;
+         result++;
+      }
+   }
+
+   return result;
+}
+
 
 console.log(countSubstrings('fdsklf'))
